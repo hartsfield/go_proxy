a96ec3be2f014f91237da9e1fbba575897511010
hartsfield <johnathanhartsfield@gmail.com>
2023-06-16 07:59:03 -0400

    updates

--- restart-service.sh
+++ restart-service.sh
@@ -4,9 +4,10 @@ export fullchain=~/tlsCerts/fullchain
 export prox80=8080
 export prox443=8443
 export proxConf=~/prox.conf
-trap -- '' SIGTERM
+# trap -- '' SIGTERM
 git pull
 go build -o go_proxy
 pkill -f go_proxy
-nohup ./go_proxy > /dev/null & disown
+# nohup ./go_proxy > /dev/null & disown
+./go_proxy
 sleep 2
