3a3aa8cb4703232eff5032ac3f317ec721da9374
John Hartsfield <john@unilink.local>
2024-12-20 01:05:05 -0500

    updates

--- init.go
+++ init.go
@@ -17,7 +17,7 @@ type config struct {
 	LiveDir      string                  `json:"live_dir"`
 	StageDir     string                  `json:"stage_dir"`
 	CertDir      string                  `json:"cert_dir"`
-	TlsCerts     *tlsCerts               `json:"tls_certs"`
+	TlsCerts     tlsCerts                `json:"tls_certs"`
 	ServiceRepos []string                `json:"service_repos"`
 	Services     map[string]*serviceConf `json:"services"`
 }
