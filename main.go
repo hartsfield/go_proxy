96f91be6e3939c2c0dee22c30d4928e4b84a7fc8
John Hartsfield <john@unilink.local>
2024-12-20 02:54:43 -0500

    updates

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func main() {
 		for {
 			_, remoteaddr, err := ser.ReadFromUDP(p)
 			fmt.Printf("Read a message from %v %s \n", remoteaddr, p)
-			if string(p) == "quit" {
+			if string(p)[:3] == "quit" {
 				fmt.Println(string(p))
 				os.Exit(0)
 			}
